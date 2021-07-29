0c81944bb4665cd118486d3f492dd4077d87da4e
ahmed reda <zaza090977777@gmail.com>
2021-07-29 18:47:54 +0200

    iam uploading my fills now

---
+++ js/plagen.js
@@ -0,0 +1,21 @@
+$(function () {
+      
+    
+        
+   
+     $('html').niceScroll();
+
+       AOS.init();
+    
+  var containerEl = document.querySelector('#containermix');
+  var mixer = mixitup(containerEl);
+    
+ 
+
+
+    
+   
+});
+  
+
+
